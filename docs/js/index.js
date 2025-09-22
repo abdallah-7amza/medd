0f27e6f345ccdafbaed477d513694473090fea8c
abdallah-7amza <abdallah7amza@gmail.com>
2025-09-22 08:47:45 -0400

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -20,15 +20,11 @@ document.addEventListener('DOMContentLoaded', async function() {
             const university = data.tree[uniId];
 
             const card = document.createElement('a');
-            card.href = 'lessons-list.html';
-            card.className = 'card';
-            card.innerHTML = `<h2>${university.name}</h2>`;
-
-            card.addEventListener('click', function(event) {
-                event.preventDefault(); 
-                localStorage.setItem('selectedUni', uniId);
-                window.location.href = card.href;
-            });
+           card.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    localStorage.setItem('selectedUni', uniId);
+    window.location.href = `lessons-list.html?path=/${uniId}`;
+});
 
             cardContainer.appendChild(card);
         }
